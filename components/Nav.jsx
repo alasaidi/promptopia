d1064e5dd5bc1885dd0174764493d97396e210a6
alasaidi <alasaidi55gmail.com>
2024-10-24 11:39:40 +0200

    creating navigation bar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,6 +6,7 @@ import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 function Nav() {
   const isUserLoggedIn = true;
   const [providers, setProvider] = useState(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
     const setProvider = async () => {
@@ -34,6 +35,42 @@ function Nav() {
             <Link href="/profile">
               <Image src="/assets/images/profile.svg" width={37} height={37} className="rounded-full" alt="profile" />
             </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button type="button" key={provider.name} onClick={() => signIn(provider.id)} className="black_btn">
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+      {/* Mobile Navigation */}
+      <div className="sm:hidden flex relative">
+        {isUserLoggedIn ? (
+          <div className="flex">
+            <Image src="/assets/images/profile.svg" width={37} height={37} className="rounded-full" alt="profile" onClick={() => setToggleDropdown((prev) => !prev)} />
+            {toggleDropdown && (
+              <div className="dropdown">
+                <Link href="/profile" className="dropdown_link" onClick={() => setToggleDropdown(false)}>
+                  My Profile
+                </Link>
+                <Link href="/create-prompt" className="dropdown_link" onClick={() => setToggleDropdown(false)}>
+                  Create Prompt
+                </Link>
+                <button
+                  className="mt-5 w-full black_btn"
+                  type="button"
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}>
+                  Sign Out
+                </button>
+              </div>
+            )}
           </div>
         ) : (
           <>
