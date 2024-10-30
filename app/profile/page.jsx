6f00fb2189a1eb1a0b689bd724909a5f3040d9ad
alasaidi <alasaidi55gmail.com>
2024-10-30 17:45:16 +0100

    setting up profile page

---
+++ app/profile/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Profile from "@components/Profile";
+function MyProfile() {
+  const handleEdit = () => {};
+  const handleDelete = async () => {};
+  return <Profile name="My" desc="Welcome to your personalized profile page" data={[]} handleEdit={handleEdit} handleDelete={handleDelete} />;
+}
+
+export default MyProfile;
