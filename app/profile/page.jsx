3f14b045ed5a2e1eada18e0522305fb5d4ae30af
alasaidi <alasaidi55gmail.com>
2024-11-02 21:05:15 +0100

    create delete function

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -15,15 +15,30 @@ function MyProfile() {
       const response = await fetch(`/api/users/${session?.user.id}/posts`);
       const data = await response.json();
       setPosts(data);
-      console.log(data);
     };
 
     if (session?.user.id) fetchPosts();
-  }, []);
+  }, [session?.user.id]);
   const handleEdit = (post) => {
     router.push(`/update-prompt?id=${post._id}`);
   };
-  const handleDelete = async (post) => {};
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm("Are You sure you want to delete this prompts?");
+    if (hasConfirmed) {
+      try {
+        const url = `/api/prompt/${post._id.toString()}`;
+        console.log("Deleting at URL:", url);
+        await fetch(url, {
+          method: "DELETE",
+        });
+        const filteredPosts = posts.filter((item) => item._id !== post._id);
+
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
   return <Profile name="My" desc="Welcome to your personalized profile page" data={posts} handleEdit={handleEdit} handleDelete={handleDelete} />;
 }
 
